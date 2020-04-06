22684a07088030e6b14c9bac45b00a81cd6e2e5a
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2020-04-06 20:41:40 +0100

    Got converter working

--- web/write.js
+++ web/write.js
@@ -31,9 +31,107 @@ class RomanisationConverter {
             return "";
         }
 
+        if (text.substr(marker.p, 1) == " ") {
+            marker.p += 1;
+            return " ";
+        }
+
         if (text.substr(marker.p, 1) == "a") {
             marker.p += 1;
             return String.fromCharCode(0x1820);
+        }
+        if (text.substr(marker.p, 1) == "e") {
+            marker.p += 1;
+            return String.fromCharCode(0x185d);
+        }
+        if (text.substr(marker.p, 1) == "i") {
+            marker.p += 1;
+            return String.fromCharCode(0x1873);
+        }
+        if (text.substr(marker.p, 1) == "o") {
+            marker.p += 1;
+            return String.fromCharCode(0x1823);
+        }
+        if (text.substr(marker.p, 1) == "u") {
+            marker.p += 1;
+            return String.fromCharCode(0x1860);
+        }
+        if (text.substr(marker.p, 1) == "y") {
+            marker.p += 1;
+            return String.fromCharCode(0x185f);
+        }
+        
+        if (text.substr(marker.p, 2) == "ng") {
+            marker.p += 2;
+            return String.fromCharCode(0x1829);
+        }
+        if (text.substr(marker.p, 1) == "n") {
+            marker.p += 1;
+            return String.fromCharCode(0x1828);
+        }
+        if (text.substr(marker.p, 1) == "k") {
+            marker.p += 1;
+            return String.fromCharCode(0x1874);
+        }
+        if (text.substr(marker.p, 1) == "g") {
+            marker.p += 1;
+            return String.fromCharCode(0x1864);
+        }
+        if (text.substr(marker.p, 1) == "h") {
+            marker.p += 1;
+            return String.fromCharCode(0x1865);
+        }
+        if (text.substr(marker.p, 1) == "b") {
+            marker.p += 1;
+            return String.fromCharCode(0x182a);
+        }
+        if (text.substr(marker.p, 1) == "p") {
+            marker.p += 1;
+            return String.fromCharCode(0x1866);
+        }
+        if (text.substr(marker.p, 2) == "sh") {
+            marker.p += 2;
+            return String.fromCharCode(0x1867);
+        }
+        if (text.substr(marker.p, 1) == "s") {
+            marker.p += 1;
+            return String.fromCharCode(0x1830);
+        }
+        if (text.substr(marker.p, 1) == "t") {
+            marker.p += 1;
+            return String.fromCharCode(0x1868);
+        }
+        if (text.substr(marker.p, 1) == "d") {
+            marker.p += 1;
+            return String.fromCharCode(0x1869);
+        }
+        if (text.substr(marker.p, 1) == "l") {
+            marker.p += 1;
+            return String.fromCharCode(0x182f);
+        }
+        if (text.substr(marker.p, 1) == "m") {
+            marker.p += 1;
+            return String.fromCharCode(0x182e);
+        }
+        if (text.substr(marker.p, 1) == "c") {
+            marker.p += 1;
+            return String.fromCharCode(0x1834);
+        }
+        if (text.substr(marker.p, 1) == "j") {
+            marker.p += 1;
+            return String.fromCharCode(0x1835);
+        }
+        if (text.substr(marker.p, 1) == "r") {
+            marker.p += 1;
+            return String.fromCharCode(0x1875);
+        }
+        if (text.substr(marker.p, 1) == "f") {
+            marker.p += 1;
+            return String.fromCharCode(0x1876);
+        }
+        if (text.substr(marker.p, 1) == "v") {
+            marker.p += 1;
+            return String.fromCharCode(0x1838);
         }
 
         marker.p += 1;
